d07cb7612bbc01a67b964e525ad9af2d02e62ebe
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-08-09 09:33:21 -0400

    SPA demo app created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import { Link,  IndexLink  } from 'react-router';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>Simple SPA</h1>
+        <ul className="header">
+          <li><IndexLink  to="/" activeClassName="active">Home</IndexLink></li>
+          <li><Link to="/info" activeClassName="active">Info</Link></li>
+          <li><Link to="/graphs" activeClassName="active">Graphs</Link></li>
+        </ul>
+        <div className="content">
+          {this.props.children}
+        </div>
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,18 @@ require('../styles/application.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import Home from './Home.jsx';
+import Info from './Info.jsx';
+import Graphs from './Graphs.jsx';
+import { Router, Route, hashHistory,IndexRoute} from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="info" component={Info} />
+      <Route path="graphs" component={Graphs} />
+    </Route>
+  </Router>,
+  document.getElementById('react-root')
+);
